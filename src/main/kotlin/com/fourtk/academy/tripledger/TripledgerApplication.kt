0bf7252c731095297887f6d9f1423ea8a0896c03
Dev-JeanSantos <jeancbsan@gmail.com>
2026-02-22 14:00:51 -0300

    starter project

---
+++ src/main/kotlin/com/fourtk/academy/tripledger/TripledgerApplication.kt
@@ -0,0 +1,11 @@
+package com.fourtk.academy.tripledger
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TripledgerApplication
+
+fun main(args: Array<String>) {
+	runApplication<TripledgerApplication>(*args)
+}
---
+++ src/test/kotlin/com/fourtk/academy/tripledger/TripledgerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.fourtk.academy.tripledger
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TripledgerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
